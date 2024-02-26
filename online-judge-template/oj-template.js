50d641385c5026e631e38bc899a20b58e7bcbfcc
JENKAI LIU <kenny0408237@gmail.com>
2024-02-26 18:17:54 +0800

    add a oj-template

---
+++ online-judge-template/oj-template.js
@@ -0,0 +1,26 @@
+var readline = require('readline');
+
+var lines = []
+var rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', function (line) {
+  lines.push(line)
+});
+
+rl.on('close', function() {
+  solve(lines)
+})
+
+function solve(lines) {
+    [a,b] = lines[0].split(' '); // ['1','1']
+    [c,d] = lines[1].split(' '); // ['2','3']
+    [e,f] = lines[2].split(' '); // ['0','0']
+}
+
+// solve([
+//     '1 1',
+//     '2 3',
+//     '0 0'     // a=b=0 means END
+// ]); 
